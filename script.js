d6afe473f1e03088a6af7ada4ad663bfcb588e96
bobbycrooz <you@example.com>
2020-07-22 17:18:36 -0700

    added active

---
+++ script.js
@@ -0,0 +1,11 @@
+var getId = document.getElementsByClassName("tootlip");
+var tootlip = getId[0];
+
+
+function showSocialsitePane(){
+    tootlip.style.visibility = "visible";
+}
+
+function closeSocialsitePane(){
+    tootlip.style.visibility = "hidden";
+}
